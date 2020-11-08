bccc0ff71efde3e9cd4cdc6e369573f4a6754813
xTrimy <Mohamed.ashraf881999@gmail.com>
2020-11-09 00:16:38 +0200

    Main website header

--- index.php
+++ index.php
@@ -2,9 +2,27 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <!-- Product Sans Font -->
+    <link rel="stylesheet" href="layout/css/productsans.css">
+    <!-- Main CSS File -->
     <link rel="stylesheet" href="layout/css/master.css">
     <title>Hikingify</title>
   </head>
   <body>
+    <!-- Header START -->
+    <div id="header">
+      <div class="logo">
+        <img src="layout/svg/logo.svg" alt="Hikingify Logo">
+      </div>
+      <div class="navigation">
+          <div> <a href="#">Home</a> </div>
+          <div> <a href="#">Destination</a> </div>
+          <div> <a href="#">Blog</a> </div>
+          <div> <a href="#">Currency EGP £</a> </div>
+          <div id="lang"> Lang </div>
+          <div> <a href="#">Log In</a> </div>
+      </div>
+    </div>
+    <!-- Header END -->
   </body>
 </html>
