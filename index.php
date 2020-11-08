041daa95df1e2a04a602c50395f5b1fd16da12e5
xTrimy <Mohamed.ashraf881999@gmail.com>
2020-11-08 23:34:38 +0200

    Organizing directory layout

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="layout/css/master.css">
+    <title>Hikingify</title>
+  </head>
+  <body>
+  </body>
+</html>
