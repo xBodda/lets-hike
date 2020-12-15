1b167bdd081781165c30579e3ba0bb04f0f34d19
xTrimy <Mohamed.ashraf881999@gmail.com>
2020-12-15 04:49:09 +0200

    Add hikes slider section to homepage

--- index.php
+++ index.php
@@ -41,7 +41,49 @@
       </div>
     </div>
     <!-- Top Banner END -->
-    
+    <!-- Hikes Slider START  -->
+
+    <div class="heading-line">
+      Top 10 Recmmendations
+    </div>
+    <div class="hikes-slide">
+      <div class="slider" id="hikes-slider">
+        <?php for($i = 0; $i<10;$i++){
+          ?>
+          <div class="item">
+            <div class="title">MT Charleston Peak, USA</div>
+            <div class="image">
+              <img src="layout/png/<?php echo $i%4 +1; ?>.png">
+            </div>
+          </div>
+          <?php
+        } ?>
+      </div>
+      <!-- Hike Slider Function TO DO HERE -->
+    </div>
+    <!-- Hike Slider Description -->
+    <div class="hike-s-description">
+      <div class="background-text" id="h-bg-text">
+        MT Charleston Peak, USA
+      </div>
+      <div class="flex-container">
+        <div class="left">
+          <div class="selected-hike-image">
+            <div class="title">MT Charleston Peak, USA</div>
+            <img src="layout/png/1.png">
+          </div>
+        </div>
+        <div class="right">
+          <h1 id="h-text">MT Charleston Peak, USA</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
+      </div>
+    </div>
+    <!-- Hikes Slider END  -->
+
     <!-- Footer START -->
     <?php include('includes/footer.php'); ?>
     <!-- Footer END -->
