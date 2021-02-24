3323752cc66a9ed3a57ad44a15a27b749cf83a8b
Muhammad Tarek <68518117+MuhammadTarekk@users.noreply.github.com>
2021-02-25 01:56:54 +0200

    Add Blog Page

---
+++ blog.php
@@ -0,0 +1,120 @@
+  <?php include('includes/head.php'); 
+  
+?>
+  <!DOCTYPE html>
+  <html lang="en" dir="ltr">
+
+  <head>
+    <meta charset="utf-8">
+    <!-- Product Sans Font -->
+    <link rel="stylesheet" href="layout/css/productsans.css">
+    <!-- Main CSS File -->
+    <link rel="stylesheet" href="layout/css/master.css">
+    <!-- Favicon  -->
+    <link href="layout/svg/logo-mark.svg" rel="shortcut icon" type="image/png">
+    <!-- Link To Icons File -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+    <title>Hikingify | Book Hike</title>
+  </head>
+
+  <body id="blog">
+    <!-- Header START -->
+    <?php include('includes/header.php'); ?>
+    <!-- Header END -->
+
+    <!-- Top Banner START -->
+    <div class="top-banner small">
+      <div class="overlay"></div>
+      <div class="content">
+        <h1>Blog</h1>
+      </div>
+    </div>
+    <!-- Top Banner END -->
+    <div>
+
+
+
+    <div id="blogPageContainer">
+    <div id="blog-flex1">
+        <div  class="Blog-Container">
+        <div class="blog-image"> <img class="blog-image-inner" src="hikemountain.jpg"> </div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+        <div >
+            <button type="button" class="blogbutton" >READ MORE</button>
+
+        </div>
+    </div>
+
+    <div class="Blog-Container">
+    <div class="blog-image"> <img class="blog-image-inner" src="hikemountain.jpg"> </div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+            <div >
+            <button type="button" class="blogbutton" >READ MORE</button>
+
+        </div>
+    </div>
+
+    <div  class="Blog-Container">
+    <div class="blog-image"> <img class="blog-image-inner" src="hikemountain.jpg"> </div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+            <div >
+            <button type="button" class="blogbutton" >READ MORE</button>
+
+        </div>
+    </div>
+
+    <div  class="Blog-Container">
+    <div class="blog-image"> <img class="blog-image-inner" src="hikemountain.jpg"> </div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+            <div >
+            <button type="button" class="blogbutton" >READ MORE</button>
+
+        </div>
+    </div>
+  </div>
+  <div id="blog-flex2">
+
+    <div  class="Blog-Container">
+    <div class="blog-image"> <img class="blog-image-inner" src="hikemountain.jpg"> </div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+            <div >
+            <button type="button" class="blogbutton" >READ MORE</button>
+
+        </div>
+    </div>
+
+    <div class="Blog-Container">
+    <div class="blog-image"> <img class="blog-image-inner" src="hikemountain.jpg"> </div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+            <div >
+            <button type="button" class="blogbutton" >READ MORE</button>
+
+        </div>
+    </div>
+
+    <div  class="Blog-Container">
+    <div class="blog-image"> <img class="blog-image-inner" src="hikemountain.jpg"> </div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+            <div >
+            <button type="button" class="blogbutton" >READ MORE</button>
+
+        </div>
+    </div>
+
+    <div  class="Blog-Container">
+    <div class="blog-image"> <img class="blog-image-inner" src="hikemountain.jpg"> </div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+            <div >
+            <button type="button" class="blogbutton" >READ MORE</button>
+
+        </div>
+    </div>
+    </div>
+
+    </div>
+
+    <!--Footer start-->
+    <?php include('includes/footer.php'); ?>
+  </body>
+
+  </html
